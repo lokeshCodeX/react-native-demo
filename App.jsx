8b352d41926d3ac0dd07c7fef162e90aed2d60a4
lokesh-capri <jadounthakur6497@gmail.com>
2025-05-01 00:02:42 +0530

    start demo project

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>ComponentName</Text>
+    </View>
+  );
+};
+
+export default App;
+
